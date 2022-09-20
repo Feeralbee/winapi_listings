aa5462c9c7ca46c06b0b88b686ceb56f166224a0
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2022-09-20 18:00:52 +0300

    completed listing 14

---
+++ list14/src/main.cpp
@@ -0,0 +1,85 @@
+#ifdef UNICODE
+#define UNICODE
+#endif
+
+#include <Windows.h>
+#include <tchar.h>
+
+TCHAR WinName[] = _T("MainFrame");
+
+TCHAR str[] = _T("Наклонный текст");
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	HFONT oldFont, newFont;
+	int Escapement;
+	static int sx, sy;
+	switch (message)
+	{
+	case WM_SIZE:
+		sx = LOWORD(lParam) / 2;
+		sy = HIWORD(lParam) / 2;
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		for (Escapement = 0; Escapement < 3600; Escapement += 200)
+		{
+			newFont = CreateFont(20, 0, Escapement, 0, 700, 1, 0, 0,
+				DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+				DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, _T("Arial"));
+			oldFont = (HFONT)SelectObject(hdc, newFont);
+			TextOut(hdc, sx, sy, str, int(_tcsclen(str)));
+			SelectObject(hdc, oldFont);
+			DeleteObject(newFont);
+		}
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY: PostQuitMessage(0); break;
+	default: return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE This, // Дескриптор текущего приложения
+	HINSTANCE, // В современных системах всегда 0
+	LPTSTR, // Командная строка
+	int mode) // Режим отображения окна
+{
+	HWND hWnd; // Дескриптор главного окна программы
+	MSG msg; // Структура для хранения сообщения
+	WNDCLASS wc; // Класс окна
+	// Определение класса окна
+	wc.hInstance = This;
+	wc.lpszClassName = WinName; // Имя класса окна
+	wc.lpfnWndProc = WndProc; // Функция окна
+	wc.style = CS_HREDRAW | CS_VREDRAW; // Стиль окна
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // Стандартная иконка
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); // Стандартный курсор
+	wc.lpszMenuName = NULL; // Нет меню
+	wc.cbClsExtra = 0; // Нет дополнительных данных класса
+	wc.cbWndExtra = 0; // Нет дополнительных данных окна
+	// Заполнение окна белым цветом
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	if (!RegisterClass(&wc)) return 0; // Регистрация класса окна
+	// Создание окна
+	hWnd = CreateWindow(WinName, // Имя класса окна
+		_T("List14"), // Заголовок окна
+		WS_OVERLAPPEDWINDOW, // Стиль окна
+		CW_USEDEFAULT,// x
+		CW_USEDEFAULT,// y Размеры окна
+		CW_USEDEFAULT,// Width
+		CW_USEDEFAULT,// Height
+		HWND_DESKTOP, // Дескриптор родительского окна
+		NULL, // Нет меню
+		This, // Дескриптор приложения
+		NULL); // Дополнительной информации нет
+	ShowWindow(hWnd, mode); //Показать окно
+	// Цикл обработки сообщений
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);// Функция трансляции кодов нажатой клавиши
+		DispatchMessage(&msg); // Посылает сообщение функции WndProc()
+	}
+	return 0;
+}
