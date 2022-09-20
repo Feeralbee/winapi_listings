3c802d0479333bfc66ae9af262a5da525c1fa2cf
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2022-09-20 17:15:34 +0300

    completed listing 3

---
+++ list3/src/main.cpp
@@ -0,0 +1,76 @@
+#ifdef UNICODE
+#define UNICODE
+#endif
+
+#include <Windows.h>
+#include <tchar.h>
+#include <string>
+
+TCHAR WinName[] = _T("MainFrame");
+
+typedef std::basic_string<TCHAR, std::char_traits<TCHAR>,
+	std::allocator<TCHAR> > String;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	static String str;
+	switch (message)
+	{
+	case WM_CHAR:
+		str += (TCHAR)wParam;
+		InvalidateRect(hWnd, NULL, TRUE);
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		TextOut(hdc, 0, 0, str.data(), int(str.size()));
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY: PostQuitMessage(0); break;
+	default: return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE This, // Дескриптор текущего приложения
+	HINSTANCE, // В современных системах всегда 0
+	LPTSTR, // Командная строка
+	int mode) // Режим отображения окна
+{
+	HWND hWnd; // Дескриптор главного окна программы
+	MSG msg; // Структура для хранения сообщения
+	WNDCLASS wc; // Класс окна
+	// Определение класса окна
+	wc.hInstance = This;
+	wc.lpszClassName = WinName; // Имя класса окна
+	wc.lpfnWndProc = WndProc; // Функция окна
+	wc.style = CS_HREDRAW | CS_VREDRAW; // Стиль окна
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // Стандартная иконка
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); // Стандартный курсор
+	wc.lpszMenuName = NULL; // Нет меню
+	wc.cbClsExtra = 0; // Нет дополнительных данных класса
+	wc.cbWndExtra = 0; // Нет дополнительных данных окна
+	// Заполнение окна белым цветом
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	if (!RegisterClass(&wc)) return 0; // Регистрация класса окна
+	// Создание окна
+	hWnd = CreateWindow(WinName, // Имя класса окна
+		_T("List3"), // Заголовок окна
+		WS_OVERLAPPEDWINDOW, // Стиль окна
+		CW_USEDEFAULT,// x
+		CW_USEDEFAULT,// y Размеры окна
+		CW_USEDEFAULT,// Width
+		CW_USEDEFAULT,// Height
+		HWND_DESKTOP, // Дескриптор родительского окна
+		NULL, // Нет меню
+		This, // Дескриптор приложения
+		NULL); // Дополнительной информации нет
+	ShowWindow(hWnd, mode); //Показать окно
+	// Цикл обработки сообщений
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);// Функция трансляции кодов нажатой клавиши
+		DispatchMessage(&msg); // Посылает сообщение функции WndProc()
+	}
+	return 0;
+}
