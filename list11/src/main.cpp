3dab8e62f2da97cd1c02f45dc93ef151d0668281
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2022-09-20 17:38:18 +0300

    completed listing 11

---
+++ list11/src/main.cpp
@@ -0,0 +1,96 @@
+#ifdef UNICODE
+#define UNICODE
+#endif
+
+#include <Windows.h>
+#include <tchar.h>
+
+
+TCHAR WinName[] = _T("MainFrame");
+
+RECT pRect = { -100, -100, 100, 100 };
+RECT pEllips = { -120, -80, 120, 80 };
+RECT pSm = { -60, -40, 60, 40 };
+const int WIDTH = 400;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	static int sx, sy;
+	HRGN hRgnEllipse;
+	HRGN hRgn;
+	static HBRUSH hBrush;
+	switch (message)
+	{
+	case WM_SIZE:
+		sx = LOWORD(lParam);
+		sy = HIWORD(lParam);
+		break;
+	case WM_CREATE:
+		hBrush = CreateSolidBrush(RGB(0, 0, 255));
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		SetMapMode(hdc, MM_ANISOTROPIC);
+		SetWindowExtEx(hdc, WIDTH, -WIDTH, NULL);
+		SetViewportExtEx(hdc, sx, sy, NULL);
+		SetViewportOrgEx(hdc, sx / 2, sy / 2, NULL);
+		hRgn = CreateRectRgnIndirect(&pRect);
+		hRgnEllipse = CreateEllipticRgnIndirect(&pEllips);
+		CombineRgn(hRgn, hRgn, hRgnEllipse, RGN_DIFF);
+		DeleteObject(hRgnEllipse);
+		hRgnEllipse = CreateEllipticRgnIndirect(&pSm);
+		CombineRgn(hRgn, hRgn, hRgnEllipse, RGN_OR);
+		DeleteObject(hRgnEllipse);
+		FillRgn(hdc, hRgn, hBrush);
+		DeleteObject(hRgn);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY: PostQuitMessage(0); break;
+	default: return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE This, // Дескриптор текущего приложения
+	HINSTANCE, // В современных системах всегда 0
+	LPTSTR, // Командная строка
+	int mode) // Режим отображения окна
+{
+	HWND hWnd; // Дескриптор главного окна программы
+	MSG msg; // Структура для хранения сообщения
+	WNDCLASS wc; // Класс окна
+	// Определение класса окна
+	wc.hInstance = This;
+	wc.lpszClassName = WinName; // Имя класса окна
+	wc.lpfnWndProc = WndProc; // Функция окна
+	wc.style = CS_HREDRAW | CS_VREDRAW; // Стиль окна
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // Стандартная иконка
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); // Стандартный курсор
+	wc.lpszMenuName = NULL; // Нет меню
+	wc.cbClsExtra = 0; // Нет дополнительных данных класса
+	wc.cbWndExtra = 0; // Нет дополнительных данных окна
+	// Заполнение окна белым цветом
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	if (!RegisterClass(&wc)) return 0; // Регистрация класса окна
+	// Создание окна
+	hWnd = CreateWindow(WinName, // Имя класса окна
+		_T("List11"), // Заголовок окна
+		WS_OVERLAPPEDWINDOW, // Стиль окна
+		CW_USEDEFAULT,// x
+		CW_USEDEFAULT,// y Размеры окна
+		CW_USEDEFAULT,// Width
+		CW_USEDEFAULT,// Height
+		HWND_DESKTOP, // Дескриптор родительского окна
+		NULL, // Нет меню
+		This, // Дескриптор приложения
+		NULL); // Дополнительной информации нет
+	ShowWindow(hWnd, mode); //Показать окно
+	// Цикл обработки сообщений
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);// Функция трансляции кодов нажатой клавиши
+		DispatchMessage(&msg); // Посылает сообщение функции WndProc()
+	}
+	return 0;
+}
